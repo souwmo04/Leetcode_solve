e373c131f0397c596a12e807f3f2cb2f173db41b
Fahim Foysal <kingfahim214@gmail.com>
2025-05-16 23:40:31 +0600

    11th

---
+++ 2901.py
@@ -0,0 +1,35 @@
+from typing import List
+
+class Solution:
+    def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        n = len(groups)
+        dp = []
+        for x in words:
+            dp.append([x])
+        
+        def hamming(a, b):
+            count = 0
+            for i in range(len(a)):
+                if a[i] != b[i]:
+                    count += 1
+                    if count > 1:
+                        return False
+            return count == 1
+        
+        mx = 1
+        res = [words[0]]
+
+        for j in range(1, n):
+            for i in range(j):
+                if (
+                    groups[i] != groups[j] and
+                    len(words[i]) == len(words[j]) and
+                    hamming(words[i], words[j]) and
+                    len(dp[j]) < len(dp[i]) + 1
+                ):
+                    dp[j] = dp[i] + [words[j]]
+                    if len(dp[j]) > mx:
+                        mx = len(dp[j])
+                        res = dp[j]
+
+        return res
