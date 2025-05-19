3a1526544a7931e353db71eca6f6925dfdf8691c
Fahim Foysal <kingfahim214@gmail.com>
2025-05-19 21:22:27 +0600

    16th

--- 9 PalindromNumber.py
+++ 9 PalindromNumber.py
@@ -6,6 +6,7 @@ class Solution:
         reversed_half = 0
         while x > reversed_half:
             reversed_half = reversed_half * 10 + x % 10
-            x //= 10
+            x //= 10 
 
         return x == reversed_half or x == reversed_half // 10
+    
