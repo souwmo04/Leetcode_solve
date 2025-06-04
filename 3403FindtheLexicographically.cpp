44827495f00c7cc7ee78ec028321c0bc20f5c24e
Fahim Foysal <kingfahim214@gmail.com>
2025-06-04 23:12:13 +0600

    34th

---
+++ 3403FindtheLexicographically.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string answerString(string word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
+        }
+        int idx = 0;
+        for (int i = 1, l = 0; i < size(word); ++i) {
+            if (word[i] == word[idx + l]) {
+                ++l;
+            } else if (word[i] < word[idx + l]) {
+                l = 0;
+            } else if (word[i] > word[idx + l]) {
+                if (word[i - l] >= word[i]) {
+                    idx = i - l;
+                } else {
+                    idx = i;
+                }
+                l = 0;
+            }
+        }
+        return word.substr(idx, (size(word) - max((numFriends - 1) - idx, 0)) - idx);
+    }
+};
