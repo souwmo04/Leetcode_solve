06047a1b0e71641eca1b44e6cbcfdc6ed4043b91
kingfahim214@gmail.com <kingfahim214@gmail.com>
2025-06-29 23:11:10 +0600

    test

--- 1498.NumberofSubsequencesThatSatisfytheGivenSumCondition.cpp
+++ 1498.NumberofSubsequencesThatSatisfytheGivenSumCondition.cpp
@@ -9,7 +9,6 @@ public:
         for (int i = 1; i <= n; ++i) {
             f[i] = (f[i - 1] * 2) % mod;
         }
-        
         int ans = 0;
         for (int i = 0; i < n && nums[i] * 2 <= target; ++i) {
             int j = upper_bound(nums.begin() + i + 1, nums.end(), target - nums[i]) - nums.begin() - 1;
